1e255b4f44e0fd1b5b0e096e8421075e8e027c8f
DikarevAndrey <dias7707@gmail.com>
2018-03-12 23:19:21 +0300

    Made main pages without linking, just html

--- qSite/urls.py
+++ qSite/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ask', views.newQuestion, name='newQuestion'),
+    path('question', views.question, name='question'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
 ]
--- qSite/views.py
+++ qSite/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the qsite index.")
+  return render(request, 'qSite/index.html')
+
+def newQuestion(request):
+  return render(request, 'qSite/ask.html')
+
+def question(request):
+  return render(request, 'qSite/question.html')
+
+def signin(request):
+  return render(request, 'qSite/login.html')
+
+def signup(request):
+  return render(request, 'qSite/signup.html')
