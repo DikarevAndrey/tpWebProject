4892158dc8035381926a01c37bf73875d5a6bf33
DikarevAndrey <dias7707@gmail.com>
2018-03-25 17:12:59 +0300

    Created most useful querySets for models in managers.py.

---
+++ qSite/managers.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class UserManager(models.Manager):
+
+  def by_username(self, _username):
+    return self.filter(user__username=_username)[0]
+    # return self.get(user__username=_username)
+
+
+class TagManager(models.Manager):
+
+  def by_tag_newest(self, _tag):
+    return self.filter(name=_tag)[0].questions.all().order_by('dateTime').reverse()
+    # return self.get(name=_tag).questions.all().order_by('dateTime')
+
+class LikeManager(models.Manager):
+  pass
+
+
+class QuestionManager(models.Manager):
+
+  def newest(self):
+    return self.order_by('dateTime').reverse()
+
+  def hottest(self):
+    return self.order_by('rating').reverse()
+
+  def by_id(self, _id):
+    return self.filter(pk=_id)[0]
+
+
+class AnswerManager(models.Manager):
+  
+  def hottest(self, qid):
+    return self.filter(question__id=qid).order_by('rating').reverse()
--- qSite/models.py
+++ qSite/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from qSite.managers import *
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Basic user info")
   avatar = models.ImageField(default="img/nobody.jpg", upload_to='uploads/%Y/%m/%d/', verbose_name="Avatar image of the user")
   rating = models.IntegerField(default=0, verbose_name="Rating of the user")
+
+  objects = UserManager()
 
   def __str__(self):
     return self.user.username
@@ -13,6 +16,8 @@ class Profile(models.Model):
 
 class Tag(models.Model):
   name = models.CharField(max_length=15, verbose_name="Name of the tag")
+
+  objects = TagManager()
 
   def __str__(self):
     return self.name
@@ -22,6 +27,8 @@ class Like(models.Model):
   VALUES = ((-1, 'DISLIKE'), (1, 'LIKE'))
   author = models.ForeignKey(Profile, null=False, verbose_name="Author of the vote", on_delete=models.DO_NOTHING)
   value = models.IntegerField(choices=VALUES, verbose_name="Like or dislike", null=False)
+
+  objects = LikeManager()
 
   def __str__(self):
     return value + ' from ' + author.user.username
@@ -33,8 +40,10 @@ class Question(models.Model):
   dateTime = models.DateTimeField(default=timezone.now, verbose_name="Date and time the question was published")
   author = models.ForeignKey(Profile, null=False, verbose_name="Author of the question", on_delete=models.DO_NOTHING)
   tags = models.ManyToManyField(Tag, blank=True, verbose_name="Tags of the question")
-  likes = models.ManyToManyField(Like, blank=True, verbose_name="Likes of the question")
-  # rating = models.IntegerField(default=0, verbose_name="Votes ratio")
+  likes = models.ManyToManyField(Like, blank=True, verbose_name="Likes of the question", related_name='questions')
+  rating = models.IntegerField(default=0, verbose_name="Votes ratio")
+
+  objects = QuestionManager()
 
   def __str__(self):
     return self.text
@@ -46,7 +55,10 @@ class Answer(models.Model):
   author = models.ForeignKey(Profile, null=False, verbose_name="Author of the answer", on_delete=models.DO_NOTHING)
   dateTime = models.DateTimeField(default=timezone.now, verbose_name="Date and time the answer was published")
   likes = models.ManyToManyField(Like, blank=True, verbose_name="Likes of the question")
-  # rating = models.IntegerField(default=0, verbose_name="Votes ratio")
+  rating = models.IntegerField(default=0, verbose_name="Votes ratio")
+  correct = models.BooleanField(default=False, verbose_name="If answer is marked as correct")
+
+  objects = AnswerManager()
 
   def __str__(self):
     return self.text
